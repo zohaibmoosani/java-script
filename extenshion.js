5d79edfeea8671eed39180c15cff080d55efa9ae
zohaibmoosani <101856801+zohaibmoosani@users.noreply.github.com>
2022-04-19 17:50:02 +0500

    Add files via upload
    marksheet

---
+++ extenshion.js
@@ -0,0 +1,66 @@
+
+// var h = parseFloat(prompt("enter value of "))
+// var r = parseFloat(prompt("enter valye of  "))
+
+
+// var result= (3.14 * r) + (  r + ( h*h ) + (r*r))
+// document.write(" <h1> result is : " + result + "</h1>");
+
+
+// var h = parseFloat(prompt("enter value of "))
+// var r = parseFloat(prompt("enter valye of  "))
+
+
+// var result= (2*3.14 * r * h) + ( 2 * 3.14 + (r*r))
+// document.write(" <h2> result is : " + result + "</h2>");
+
+
+// var h = parseFloat(prompt("enter value of "))
+// var r = parseFloat(prompt("enter valye of  "))
+
+
+// var result= (3.14 * (r * r) * h) 
+// document.write(" <h3> result is : " + result + "</h3>");
+
+
+// var vf = parseFloat(prompt("enter value of "))
+// var vi = parseFloat(prompt("enter value of  "))
+// var vf = parseFloat(prompt("enter value of "))
+// var vi = parseFloat(prompt("enter value of  "))
+
+
+// var result= (vf*vf)-(vi * vi) / (2 * a)
+// document.write(" <h3> result is : " + result + "</h3>");
+
+// var a = parseFloat(prompt("enter value of "))
+// var b = parseFloat(prompt("enter valye of  "))
+
+
+// var result= (a*a*a) + (3*a*a*b)+(3*a*a*b)+(b*b*b)
+// document.write(" <h3> result is : " + result + "</h3>")
+
+
+// var vi = parseFloat(prompt("enter value of "))
+// var t = parseFloat(prompt("enter valye of  "))
+// var a = parseFloat(prompt("enter valye of  "))
+
+
+
+// var result= (vi*t) + ( (1*a*t*t) / 2)
+// document.write(" <h3> result is : " + result + "</h3>")
+
+
+
+var name = prompt("what is your  name?") 
+var a =  parseFloat (prompt("what is your  english marks?") )
+var b = parseFloat(prompt("what is your  computer marks?") )
+var c = parseFloat(prompt("what is your  maths marks?") )
+var d = parseFloat(prompt("what is your  chemistry marks?") )
+var e= parseFloat(prompt("what is your  urdu marks?") )
+
+var total = (a+b+c+d+e)
+document.write("<h1> " + name + " total marks is " + total + "<h1/>"  )
+
+var per = ( (total/250) * 100)
+document.write("upur percentange is " + per + "%")
+
