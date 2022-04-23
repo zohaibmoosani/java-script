9a67ad5d91410ee0005a513ac33dc6f68a562729
zohaibmoosani <101856801+zohaibmoosani@users.noreply.github.com>
2022-04-23 17:28:24 +0500

    Add files via upload
    marekseht

---
+++ studentmarkheet.js
@@ -0,0 +1,45 @@
+// var physic = parseInt(prompt("total marks"))
+// var urdu  = parseInt(prompt("total marks"))
+// var chemistry = parseInt(prompt( " total marks"))
+// var bio = parseInt(prompt("total marks"))
+// var english = parseInt(prompt("total marks"))
+
+// var total = physic  + urdu +  chemistry +  bio + english 
+// var  percantage =  (total * 100 ) / 500  
+
+// document.write("total" + total   + "</br> <hr/>")
+// document.write("total" + percantage +"%"+ "</br> <hr/>" )
+
+
+// if(percantage >=80)
+// {
+//     document.write("you got A+")
+// }
+// else if (percantage >=70)
+// {
+//     document.write("you got A")
+// }
+
+// else if (percantage >=60)
+// {
+//     document.write("you got b")
+// }
+
+// else if (percantage >=50)
+// {
+//     document.write("you got c")
+// }
+// else {
+//     document.write("you are fail") 
+// }
+
+var email = prompt("enter email")
+var password = prompt("enter pwd")
+
+if (email == "admin@gmail.com" && password == "123"){
+    document.write ("welcome admin ")
+}
+else {
+    document.write ("eror 404")
+
+}
